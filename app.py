a0b784407ecd5fc49471163c76b63c6321725a80
denyfaishal25 <denyfaishalardiyan@gmail.com>
2024-11-05 14:55:05 +0700

    adding login n register form

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ def home():
 def admin_dashboard():
     return render_template('admin/dashboard-admin.html')
 
+# Route untuk halaman login
+@app.route('/login')
+def login():
+    return render_template('components/login-form.html')
+
+@app.route('/register')
+def register():
+    return render_template('components/register-form.html')
+
 # Route untuk logout
 @app.route('/logout', methods=['POST'])
 def logout():
-    # Logika logout, misalnya menghapus session
     session.clear()
-    return redirect(url_for('home')) 
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
